e45b867efb8860adf68d32c7594a6360e4526581
phmis <phmis@145.94.147.171>
2018-10-03 16:28:20 +0200

    Refactoring
    Check code to adhere to java language conventions
    Use utility funciton for parameters

--- AitGroup25Assignment/src/ai2018/group25/Group25_AS.java
+++ AitGroup25Assignment/src/ai2018/group25/Group25_AS.java
@@ -1,5 +1,7 @@
 package ai2018.group25;
 
+import static ai2018.group25.Group25_Utils.*;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -13,87 +15,79 @@ import genius.core.boaframework.OpponentModel;
 
 @SuppressWarnings("deprecation")
 public class Group25_AS extends AcceptanceStrategy {
-	
-	private static final Double RESERVATIONVALUE = 0.4; // Still to adjust with tests
-	
-	private static final Double STARTSFALLING = 0.7; // Still to adjust with tests
-	
-	private double reservationValue;
-	private double startsFalling;
 
+	private static final Double RESERVATION_VALUE_DEFAULT = 0.4; // Still to adjust with tests
+	private static final Double CONCEDE_MOMENT_DEFAULT = 0.7; // Still to adjust with tests
+
+	private double reservationValue;
+	private double concedeMoment;
 
 	public Group25_AS() {
 	}
-	
-	public Group25_AS(NegotiationSession negotiationSession, OfferingStrategy offeringStrategy, double reservationValue) {
+
+	public Group25_AS(NegotiationSession negotiationSession, OfferingStrategy offeringStrategy,
+			double reservationValue) {
 		this.negotiationSession = negotiationSession;
 		this.offeringStrategy = offeringStrategy;
 		this.reservationValue = reservationValue;
 	}
-	
+
 	@Override
 	public void init(NegotiationSession negotiationSession, OfferingStrategy offeringStrategy,
 			OpponentModel opponentModel, Map<String, Double> parameters) throws Exception {
 		this.negotiationSession = negotiationSession;
 		this.offeringStrategy = offeringStrategy;
 
-		if (parameters.get("reservationValue") != null || parameters.get("startsFalling") != null ) {
-			reservationValue = parameters.get("reservationValue");
-			startsFalling = parameters.get("startsFalling");
-		} else {
-			reservationValue = RESERVATIONVALUE;
-			startsFalling = STARTSFALLING;
-		}
+		reservationValue = getParams("reservationValue", RESERVATION_VALUE_DEFAULT, parameters);
+		concedeMoment = getParams("concedeMoment", CONCEDE_MOMENT_DEFAULT, parameters);
 	}
-	
+
 	@Override
 	public Actions determineAcceptability() {
-		// Accepting Strategy Implementation
-		
-		// Initialize variable
 		double myLastBidUtil = -1.0;
 		double rightLimit = -1.0;
 		double decisionLimit;
-		
+
 		// Get my last bid and the bid I am planning on doing next
 		if (negotiationSession.getOwnBidHistory().getLastBidDetails() != null) { // Cover the first case
 			myLastBidUtil = negotiationSession.getOwnBidHistory().getLastBidDetails().getMyUndiscountedUtil();
 		}
-		
+
 		double myNextBidUtil = offeringStrategy.getNextBid().getMyUndiscountedUtil();
-		
+
 		// The Right limit is the minimum between my last and my next bid
 		if (myLastBidUtil != -1.0) {
 			rightLimit = Math.min(myLastBidUtil, myNextBidUtil);
 		} else {
 			rightLimit = myNextBidUtil;
 		}
-		
-		if ( rightLimit <= reservationValue ) {
+
+		if (rightLimit <= reservationValue) {
 			decisionLimit = reservationValue; // Error avoidance
 		} else {
 			// Get, according to time, the percentage of the interval not to accept
-			double percentage_interval_rejected = what_reject(negotiationSession.getTime());
+			double percentageIntervalRejected = computeRejectionPercentage(negotiationSession.getTime());
 			// Compute the decision limit
-			decisionLimit = reservationValue + percentage_interval_rejected * ( rightLimit - reservationValue );
+			decisionLimit = reservationValue + percentageIntervalRejected * (rightLimit - reservationValue);
 		}
-		
+
 		// Get the utility of the bid the opponent made, and act accordingly
-		double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails().getMyUndiscountedUtil();
-		
-		if (lastOpponentBidUtil >= decisionLimit ) {
+		double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails()
+				.getMyUndiscountedUtil();
+
+		if (lastOpponentBidUtil >= decisionLimit) {
 			return Actions.Accept;
 		}
 		return Actions.Reject;
 	}
-	
+
 	@Override
 	public Set<BOAparameter> getParameterSpec() {
 
 		Set<BOAparameter> set = new HashSet<BOAparameter>();
-		set.add(new BOAparameter("reservation_value", RESERVATIONVALUE ,
+		set.add(new BOAparameter("reservation_value", RESERVATION_VALUE_DEFAULT,
 				"Reservation Value, never accept offers below this value of utility"));
-		set.add(new BOAparameter("starts_falling", STARTSFALLING ,
+		set.add(new BOAparameter("starts_falling", CONCEDE_MOMENT_DEFAULT,
 				"Threshold before starting to accept bids near the reservation value"));
 		return set;
 	}
@@ -102,19 +96,19 @@ public class Group25_AS extends AcceptanceStrategy {
 	public String getName() {
 		return "Group 25 Acceptance Strategy";
 	}
-	
+
 	@Override
 	public String printParameters() {
-		return "[paramter 1: " + reservationValue + " ]";
+		return "[reservationValue: " + reservationValue + "; concedeMoment: " + concedeMoment + " ]";
 	}
-	
-	private double what_reject(double normalized_time) {
-		if ( normalized_time <= startsFalling ) { 
+
+	private double computeRejectionPercentage(double normalizedTime) {
+		if (normalizedTime <= concedeMoment) {
 			return 1.0;
 		} else {
-			return ( -1/(1-startsFalling) )*normalized_time + ( 1/(1-startsFalling) ); // Still linear, transform into exponential
+			return (-1 / (1 - concedeMoment)) * normalizedTime + (1 / (1 - concedeMoment)); // Still linear, transform
+																							// into exponential
 		}
 	}
-	
-}
 
+}

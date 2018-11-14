bb60d3a1fc750c1bce2f0340e02ca28dee04aa34
phmis <phmis@145.94.225.103>
2018-11-14 14:40:45 +0100

    Add Boa Party

---
+++ AitGroup25Assignment/src/ai2018/group25/Group25_Party.java
@@ -0,0 +1,33 @@
+package ai2018.group25;
+
+import java.util.HashMap;
+
+import genius.core.boaframework.BoaParty;
+import genius.core.boaframework.NegotiationSession;
+import genius.core.boaframework.SessionData;
+import genius.core.parties.NegotiationInfo;
+import genius.core.persistent.PersistentDataType;
+
+public class Group25_Party extends BoaParty {
+	
+	private static final long serialVersionUID = 1L;
+
+	public Group25_Party() {
+		super(null, new HashMap<String, Double>(), null, new HashMap<String, Double>(), null,
+				new HashMap<String, Double>(), null, new HashMap<String, Double>());
+	}
+
+	@Override
+	public void init(NegotiationInfo info) {
+		opponentModel = new Group25_OM();
+		omStrategy = new Group25_OMS();
+		offeringStrategy = new Group25_BS();
+		acceptConditions = new Group25_AS();
+		super.init(info);
+	}
+
+	@Override
+	public String getDescription() {
+		return "Group 25 BOA Party ";
+	}
+}
